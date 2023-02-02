f4ae3ea429e802c400a53d2f6a1d1acdc25fac12
SergBily <89011743+SergBily@users.noreply.github.com>
2023-02-02 07:53:07 +0300

    init: start server for final project itransition

---
+++ index.js
@@ -0,0 +1,64 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import { v4 as uuidv4 } from 'uuid';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = createServer(app);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: [process.env.CLIENT_URL, process.env.DEPLOY_URL],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}));
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', [process.env.CLIENT_URL, process.env.DEPLOY_URL]);
+  res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use('/', (_req, res) => {
+  res.send('work');
+});
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const io = new Server(server, {
+  cors: [process.env.CLIENT_URL, process.env.DEPLOY_URL],
+});
+
+server.listen(PORT, () => console.log('port is work'));
+
+io.use((socket, next) => {
+  const { userName, sessionID, userID } = socket.handshake.auth;
+  if (!userName) {
+    return next(new Error('invalid username'));
+  }
+  if (sessionID) {
+    socket.sessionID = sessionID;
+    socket.userID = userID;
+    socket.userName = userName;
+    next();
+  }
+  socket.sessionID = uuidv4();
+  socket.userID = uuidv4();
+  socket.userName = userName;
+  next();
+});
+
+io.on('connection', (socket) => {
+  socket.join(socket.userName);
+});
